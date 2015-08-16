25f201ef8064aa0e0ef62bfb3b4064fa315944bd
spartaksun <2697024@gmail.com>
2015-08-16 16:20:00 +0300

    Added bundles config

---
+++ backend/src/OrientDbBundle/OrientDbBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by A.Belyakovskiy.
+ * Date: 8/16/15
+ * Time: 2:21 PM
+ */
+namespace OrientDbBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class OrientDbBundle
+ * @package OrientDbBundle
+ */
+class OrientDbBundle extends Bundle
+{
+}
